e1fcceeb3ab0335fb1cd01c077bd91184b62cb3c
josevictor <jose.victor@edu.unifor.br>
2019-04-10 19:50:30 -0300

    atualização de cores

--- app/src/main/java/br/unifor/usandomaterialdesign/BottomBarTheme.kt
+++
@@ -1,4 +0,0 @@
-package br.unifor.usandomaterialdesign
-
-class BottomBarTheme {
-}
--- app/src/main/java/br/unifor/usandomaterialdesign/MainActivity.kt
+++ app/src/main/java/br/unifor/usandomaterialdesign/MainActivity.kt
@@ -2,6 +2,7 @@ package br.unifor.usandomaterialdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
