489a9975c6e3d0ee555aea952ecd8ca1c3ae088a
josevictor <jose.victor@edu.unifor.br>
2019-04-29 01:49:17 -0300

    atualização imageview

--- app/src/main/java/br/unifor/usandomaterialdesign/MainActivity.kt
+++ app/src/main/java/br/unifor/usandomaterialdesign/MainActivity.kt
@@ -2,7 +2,6 @@ package br.unifor.usandomaterialdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
